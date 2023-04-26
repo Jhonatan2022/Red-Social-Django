fc713251d004416945d32395563de458607115a1
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2023-04-26 17:21:26 +0100

    Extend database

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+# Este documento nos servira para extender la base de datos y las tablas que deseemos
 # Importamos models para poder crear nuestras clases que heredan de models.Model
 from django.db import models
 
@@ -19,11 +20,8 @@ from django.db.models.signals import post_save
 
 
 
-#----------------------------MODELS--------------------------------------
-# Este documento nos servira para extender la base de datos y las tablas que deseemos
 
-
-
+#----------------------------VARIABLES GLOBALES----------------------------
 # Creamos una función para cuando el usuario quiera subir una imagen de perfil
 # La intancia hace referencia al modelo Profile (Usuario) y el filename al nombre del archivo
 def user_directory_path_profile(instance, filname):
@@ -31,6 +29,7 @@ def user_directory_path_profile(instance, filname):
     # Cremos un apartado para que se cree una carpeta con el nombre del usuario y dentro se guarde la imagen
     # El 0 hace referencia al nombre de usuario
     profile_picture_name = 'user/{0}/profile.jpg'.format(instance.user.username)
+
 
     # Definimos la ruta donde se guardará la imagen de perfil
     full_path = os.path.join(settings.MEDIA_ROOT, profile_picture_name)
@@ -61,11 +60,12 @@ VERIFICATION_OPTIONS = (
     # Como segunda opción tendremos la de verificado
     ('verified', 'verified'),
 )
+#----------------------------VARIABLES GLOBALES----------------------------
 
 
 
 
-
+#----------------------------MODELS--------------------------------------
 # Creamos una función para cuando el usuario quiera subir una imagen de banner
 # La intancia hace referencia al modelo Profile (Usuario) y el filename al nombre del archivo
 def user_directory_path_banner(instance, filname):
@@ -110,7 +110,91 @@ class Profile(models.Model):
     # Creamos un campo de imagen para poder añadir una imagen de perfil
     # upload_to para indicar la ruta donde se guardará la imagen
     # default para indicar una imagen por defecto
-    picture = models.ImageField(upload_to='profile', default='media/users/default_profile.jpg', upload_to=user_directory_path_profile)
+    picture = models.ImageField(default='users/default_profile.jpg', upload_to=user_directory_path_profile)
 
     # Creamos un campo para el banner del perfil del usuario (imagen de portada)
-    banner = models.ImageField(upload_to='banners', default='media/users/default_banner.jpg', upload_to=user_directory_path_banner)
+    banner = models.ImageField(default='users/default_banner.jpg', upload_to=user_directory_path_banner)
+
+    # Creamos un campo para la verificación del usuario (si es verificado o no)
+    # choices para indicar las opciones que tendrá el campo
+    # default para indicar el valor por defecto
+    verified = models.CharField(max_length=10, choices=VERIFICATION_OPTIONS, default='unverified')
+
+    # Crearemos un campo de coins para la monetización de la aplicación (moneda virtual)
+    # Usaremos (max_digits para indicar el número de dígitos y decimal_places para indicar el número de decimales)
+    # Usamos blank=False para que siempre tenga coins ya sea 0 o más
+    coins = models.DecimalField(max_digits=19, decimal_places=2, default=0, blank=False) 
+
+    # Creamos un campo para la fecha de creación del perfil (auto_now_add para que se cree automáticamente)
+    created = models.DateField(auto_now_add=True)
+
+
+    # INFORMACIÓN DEL USUARIO
+
+    # Creamos un campo donde el usuario podrá ingresar en donde se encuentra (ciudad, país, etc)
+    # Usamos null=True para que pueda ser nulo en la base de datos
+    # blank=True para que no sea obligatorio
+    location = models.CharField(max_length=50, null=True, blank=True)
+
+    # Creamos un campo para almacenar la url del perfil del usuario usuario
+    # Usamos null=True para que pueda ser nulo en la base de datos
+    # blank=True para que no sea obligatorio
+    website = models.URLField(max_length=80, null=True, blank=True)
+
+    # Creamos un campo para la fecha de cumpleaños del usuario 
+    # Usamos null=True para que pueda ser nulo en la base de datos
+    # blank=True para que no sea obligatorio
+    birthday = models.DateField(null=True, blank=True)
+
+    # Creamos un campo para la biografía del usuario
+    # Usamos null=True para que pueda ser nulo en la base de datos
+    # blank=True para que no sea obligatorio
+    biography = models.TextField(max_length=150, null=True, blank=True)
+#----------------------------MODELS--------------------------------------
+
+
+
+
+#--------------------------PARA EL PANEL DE ADMINISTRACIÓN-------------------#
+    # Definimos que información vera el administrador en el panel de administración
+    def __str__(self):
+
+        # Devolvemos el nombre de usuario
+        return self.user.username
+#--------------------------PARA EL PANEL DE ADMINISTRACIÓN-------------------#
+
+
+
+
+#-----------------------GURADANDO Y CREANDO EL PERFIL DEL USUARIO-------------#
+# Crearemos una instancia cuando el usuario se registre para crear un perfil 
+# Usamos signals para que se cree automáticamente
+# **kwargs para que acepte cualquier argumento que le pasemos (no es obligatorio)
+def create_user_profile(sender, instance, created, **kwargs):
+
+    # Si el usuario se crea
+    if created:
+
+        # Creamos un perfil para el usuario
+        Profile.objects.create(user=instance)
+
+
+
+# Cremos una función para guardar el perfil del usuario
+def save_user_profile(sender, instance, **kwargs):
+
+    # La instancia hace referencia al modelo Profile (Usuario)
+    # Guardamos el perfil del usuario
+    instance.profile.save()
+
+
+
+# Usamos post_save para que se cree el perfil después de que se guarde el usuario
+# Usamos connect para conectar la señal con la función
+post_save.connect(create_user_profile, sender=User) 
+
+
+# Usamos post_save para que se guarde el perfil después de que se guarde el usuario
+# Usamos connect para conectar la señal con la función
+post_save.connect(save_user_profile, sender=User)
+#-----------------------GURADANDO Y CREANDO EL PERFIL DEL USUARIO-------------#
