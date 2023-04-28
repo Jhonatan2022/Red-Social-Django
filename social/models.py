8e540644875d7ac340087e210107dc4aebf5ca60
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2023-04-28 22:34:47 +0100

    Created posts

--- social/models.py
+++ social/models.py
@@ -1,3 +1,22 @@
+# Importamos models para crear los modelos de la base de datos de Django
 from django.db import models
 
-# Create your models here.
+# Importamos timezone para las fechas de publicación de los posts
+from django.utils import timezone
+
+# Importamos get_user_model para obtener el modelo de usuario actual
+from django.contrib.auth import get_user_model
+#--------------------------------IMPORT BOOKSTORES---------------------
+
+
+
+# Definimos user como el modelo de usuario actual
+User = get_user_model()
+
+
+
+#--------------------------------MODELS--------------------------------
+# Definimos en donde e subiran las imagenes de los posts
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
