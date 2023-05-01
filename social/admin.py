bc7f28a5299a127298fddbf3a2b57952a9860cbc
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2023-05-01 21:46:16 +0100

    successful publication

--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+# Importamos los modelos de la aplicación social
+from .models import SocialPost, SocialComment
+
 # Register your models here.
+
+# Registramos el modelo SocialPost en el administrador de Django
+admin.site.register(SocialPost)
+
+# Registramos el modelo SocialComment en el administrador de Django
+admin.site.register(SocialComment)
